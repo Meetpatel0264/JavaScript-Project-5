bcc566f98f883eff733da608a7c7badd2d655bd2
Meetpatel0264 <patelmeet9996@gmail.com>
2025-11-21 22:42:03 +0530

    Compalte the array Project

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+function clearInput() {
+  document.getElementById("inputString").value = "";
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML = "";
+  resultElement.className = "result"; 
+}
+
+function checkPalindrome() {
+  let inputString = document.getElementById("inputString").value.toLowerCase().trim();
+  let resultElement = document.getElementById("result");
+
+   classes
+  resultElement.className = "result";
+
+  
+  inputString = inputString.replace(/[^a-z0-9]/gi, '');
+
+  if (inputString === "") {
+    resultElement.innerHTML = "⚠️ Invalid input!";
+    resultElement.classList.add("show", "empty");
+    return;
+  }
+
+  let reversed = inputString.split("").reverse().join("");
+
+  if (reversed === inputString) {
+    resultElement.innerHTML = "✅ It is a Palindrome!";
+    resultElement.classList.add("show", "palindrome");
+  } else {
+    resultElement.innerHTML = "❌ It is Not a Palindrome!";
+    resultElement.classList.add("show", "not-palindrome");
+  }
+}
